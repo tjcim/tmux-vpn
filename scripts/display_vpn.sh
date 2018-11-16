c0abf85970ad868d352f393eb90d10ddeff07cf3
Trevor Christiansen <trevor@tjc.im>
2018-11-16 11:38:12 -0700

    updated to tun0 and color change

--- scripts/display_vpn.sh
+++ scripts/display_vpn.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
-# Check if we're on the corp wifi
-network_name=$( /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport --getinfo | grep -E '\bSSID:' | cut -d: -f2- | sed -E -e 's/^ (.+)/\1/' )
-
-if netstat -nr -f inet | grep utun1 > /dev/null; then
-	echo "#[bg=colour70,fg=colour255] VPN "
-elif [[ $network_name == "wpa2" ]]; then
-	echo "#[bg=colour0,fg=colour75] WiFi "
+if netstat -nr -f inet | grep tun0 > /dev/null; then
+	echo "#[bg=colour226,fg=colour255] VPN "
 else
-	echo "#[bg=colour214,fg=colour240] VPN "
+	echo "#[bg=colour235,fg=colour136] VPN "
 fi
