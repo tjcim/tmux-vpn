7dcb3036ee44dbe82fa0e6ecbe54bf1a5f54703e
Trevor Christiansen <trevor@tjc.im>
2018-11-16 11:40:34 -0700

    color change

--- scripts/display_vpn.sh
+++ scripts/display_vpn.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if netstat -nr -f inet | grep tun0 > /dev/null; then
-	echo "#[bg=colour226,fg=colour255] VPN "
+	echo "#[bg=colour1,fg=colour235] VPN "
 else
-	echo "#[bg=colour235,fg=colour136] VPN "
+	echo "#[bg=colour235,fg=colour239] VPN "
 fi
